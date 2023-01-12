6ac007216f5d0c816810720bfc1dfbb603cf3faf
Mounir <polomnr1@gmail.com>
2023-01-12 21:16:19 +0100

    Add sign out button

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ function App() {
     <div className="App">
         <header>
               <h1>⚛️🔥💬</h1>
+              <SignOut />
         </header>
         <section>
             {user ? <ChatRoom /> : <SignIn />}
